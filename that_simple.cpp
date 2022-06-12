d433278be5368ddc25c31da7c46b1c90bdf6f9a6
Ignore <>
2022-06-12 21:04:06 +0200

    Connect core

--- that_simple.cpp
+++ that_simple.cpp
@@ -1,22 +1,28 @@
 #include "daisy_seed.h"
 #include "daisysp.h"
+#include "core/Spotykach.h"
 
 using namespace daisy;
 using namespace daisysp;
 
+using namespace vlly;
+using namespace spotykach;
+
 DaisySeed hw;
+Spotykach* core;
 
 void AudioCallback(AudioHandle::InputBuffer in, AudioHandle::OutputBuffer out, size_t size)
 {
-	for (size_t i = 0; i < size; i++)
-	{
-		out[0][i] = in[0][i];
-		out[1][i] = in[1][i];
+
+	for (size_t i = 0; i < size; i++) {
+		float** outBufs[4] = { out, nullptr, nullptr, nullptr };
+		core->process(in, false, outBufs, false, size);
 	}
 }
 
-int main(void)
-{
+int main(void) {
+	core = new Spotykach();	
+
 	hw.Init();
 	hw.SetAudioBlockSize(4); // number of samples handled per callback
 	hw.SetAudioSampleRate(SaiHandle::Config::SampleRate::SAI_48KHZ);
