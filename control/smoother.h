fab27fe8a48c4d5f8cc14bf3b28b62a1dea469e9
Vlad <thatvlad@icloud.com>
2022-10-17 21:45:02 +0200

    Add smoother

---
+++ control/smoother.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <math.h>
+
+struct Smoother {
+public:
+    Smoother() = default;
+    ~Smoother() = default;
+
+    float smoothing() const { 
+        return _smooth_k; 
+    }
+
+    void setSmoothing(float s) {
+        if (s >= 0.5 && s <= 1.f) _smooth_k = s;
+    }
+
+    float smoothed(float rough) {
+        if (_val == 0) {
+            _val = rough;
+        }
+        else {
+            auto smth = rough * _smooth_k + (1 - _smooth_k) * _val;
+            auto qntz = roundf(smth * 1024.f) / 1024.f;
+            _val = floorf(qntz * 1000.f) / 1000.f;
+        }
+        
+        return _val;
+    }
+
+private:
+    float _val      = 0;
+    float _smooth_k = 0.75;
+};
