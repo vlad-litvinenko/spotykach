5d80668ed3f20d095d323e2dccd3d9158e095cbb
Vlad <thatvlad@icloud.com>
2022-10-24 20:28:15 +0200

    Use index

--- control/knob.cpp
+++ control/knob.cpp
@@ -4,16 +4,16 @@ using namespace daisy;
 
 void Knob::initialize(AdcChannelConfig& conf, int channel) {
     _channel = channel;
-    auto target = _targets[0];
+    auto target = _targets[channel];
     conf.InitSingle(pin(target));
 }
 
 void Knob::configure(DaisySeed& hw) {
     const auto flip = true;
     _ctrl.Init(hw.adc.GetPtr(_channel), hw.AudioCallbackRate(), flip);
-};
+}
 
 float Knob::value() { 
     _ctrl.Process();
-    return _smoother.smoothed(_ctrl.Value()); 
-};
+    return  _smoother.smoothed(_ctrl.Value()); 
+}
