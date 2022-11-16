8c812db70b9422451e83863595647798bd6c0e43
Vlad <thatvlad@icloud.com>
2022-11-16 10:16:28 +0100

    Extract implemetation

---
+++ control/midisync.cpp
@@ -0,0 +1,131 @@
+#include "midisync.h"
+
+inline uint16_t combinedBytes(uint8_t bytes[2]) {
+   uint16_t _14bit;
+   _14bit = (uint16_t)bytes[1];
+   _14bit <<= 7;
+   _14bit |= (uint16_t)bytes[0];
+   return _14bit;
+}
+
+inline float calculatedBeat(uint32_t beat, uint32_t tick) {
+    return static_cast<float>(beat) + static_cast<float>(tick) / 24.f;
+}
+
+void MIDISync::handleEvent(daisy::MidiEvent e) {
+    using namespace daisy; 
+
+    if (e.type == MidiMessageType::SystemCommon) {
+        switch (e.sc_type) {
+            case SystemCommonType::SongPositionPointer: seek(e.data); break;
+            default: {};
+        }
+        return;
+    }
+
+    if (e.type == MidiMessageType::SystemRealTime) {
+        switch (e.srt_type) {
+            case SystemRealTimeType::TimingClock: tick(); break;
+            case SystemRealTimeType::Start: start(); break;
+            case SystemRealTimeType::Continue: resume(); break;
+            case SystemRealTimeType::Stop: stop(); break;
+            default: {};
+        }
+        return;
+    }
+}
+
+bool MIDISync::isPlaying() {
+    return _isPlaying;
+}
+
+float MIDISync::tempo() {
+    return _tempo;
+}
+
+float MIDISync::beat() {
+    return _beat;
+}
+    
+bool MIDISync::readAndResetSPPChanged() {
+    auto r = _isSPPChanged;
+    _isSPPChanged = false;
+    return r;
+}
+
+void MIDISync::reset() {
+    _ptime = 0;
+    _wndw.fill(0);
+    _iterator = 0;
+    _filled = false;
+    _dev_cnt = 0;
+}
+
+void MIDISync::start() {
+    _isPlaying = true;
+    _tick_cnt = 0;
+    _beat_cnt = 0;
+}
+
+void MIDISync::stop() {
+    _isPlaying = false;
+}
+
+void MIDISync::resume() {
+    _isPlaying = true;
+}
+
+void MIDISync::seek(uint8_t bytes[2]) {
+    auto beat = combinedBytes(bytes);
+    _beat_cnt = beat / 4;
+    _tick_cnt = (beat - _beat_cnt * 4) * 6;
+    _beat = calculatedBeat(_beat_cnt, _tick_cnt);
+    _isSPPChanged = true;
+}
+
+void MIDISync::tick() {
+    _tick_cnt++;
+    if (_tick_cnt == 24) {
+        _tick_cnt = 0;
+        _beat_cnt ++;
+    }
+    _beat = calculatedBeat(_beat_cnt, _tick_cnt);
+
+    auto t = daisy::System::GetNow();
+    auto delta = t - _ptime;
+    if (_ptime > 0) push(delta);
+    _ptime = t;
+    _tempo = tempo(avg());
+    checkDeviation(delta);
+}
+
+void MIDISync::push(uint32_t interval) {
+    _wndw[_iterator] = interval;
+    _iterator ++;
+    _iterator %= _wndw.size(); 
+    if (_iterator == 0) _filled = true;
+}
+
+float MIDISync::avg() {
+    float s = 0;
+    auto l = _filled ? _wndw.size() : _iterator + 1;
+    for (size_t i = 0; i < l; i++) s += _wndw[i];
+    _avg = s / l;
+    return _avg;
+}
+
+float MIDISync::tempo(float tick) {
+    return roundf(2500.f / tick);
+}
+
+void MIDISync::checkDeviation(uint32_t delta) {
+    if (delta > _avg - _dev_thres && delta < _avg + _dev_thres) {
+        _dev_cnt = 0;
+        return;
+    }
+    if (++_dev_cnt < _dev_cnt_thres) return;
+    
+    _iterator = 0;
+    _filled = false;
+    _dev_cnt = 0;
+}
--- control/midisync.h
+++ control/midisync.h
@@ -1,18 +1,6 @@
 #include "daisy_seed.h"
 #include <thread>
 
-inline uint16_t combinedBytes(uint8_t bytes[2]) {
-   uint16_t _14bit;
-   _14bit = (uint16_t)bytes[1];
-   _14bit <<= 7;
-   _14bit |= (uint16_t)bytes[0];
-   return _14bit;
-}
-
-inline float calculatedBeat(uint32_t beat, uint32_t tick) {
-    return static_cast<float>(beat) + static_cast<float>(tick) / 24.f;
-}
-
 class MIDISync {
 public:
     MIDISync() = default;
@@ -20,52 +8,12 @@ public:
 
     //This code relies on tweak in usb_midi.cpp, namely
     //removing filtering of 0xF at line 119
-    void handleEvent(daisy::MidiEvent e) {
-        if (e.type == daisy::SystemCommon) {
-            switch (e.sc_type) {
-                case daisy::SystemCommonType::SongPositionPointer: seek(e.data); break;
-                default: {};
-            }
-            return;
-        }
-
-        if (e.type == daisy::MidiMessageType::SystemRealTime) {
-            switch (e.srt_type) {
-                case daisy::SystemRealTimeType::TimingClock: tick(); break;
-                case daisy::SystemRealTimeType::Start: start(); break;
-                case daisy::SystemRealTimeType::Continue: resume(); break;
-                case daisy::SystemRealTimeType::Stop: stop(); break;
-                default: {};
-            }
-            return;
-        }
-    }
-
-    bool isPlaying() {
-        return _isPlaying;
-    }
-
-    float tempo() {
-        return _tempo;
-    }
-
-    float beat() {
-        return _beat;
-    }
-    
-    bool readAndResetSPPChanged() {
-        auto r = _isSPPChanged;
-        _isSPPChanged = false;
-        return r;
-    }
-
-    void reset() {
-        _ptime = 0;
-        _wndw.fill(0);
-        _iterator = 0;
-        _filled = false;
-        _dev_cnt = 0;
-    }
+    void handleEvent(daisy::MidiEvent e);
+    bool isPlaying();
+    float tempo();
+    float beat();
+    bool readAndResetSPPChanged();
+    void reset();
 
 private:
     bool _filled = false;
@@ -84,72 +32,13 @@ private:
     bool _isPlaying = false;
     bool _isSPPChanged = false;
     
-    void start() {
-        _isPlaying = true;
-        _tick_cnt = 0;
-        _beat_cnt = 0;
-    }
-
-    void stop() {
-        _isPlaying = false;
-    }
-
-    void resume() {
-        _isPlaying = true;
-    }
-
-    void seek(uint8_t bytes[2]) {
-        auto beat = combinedBytes(bytes);
-        _beat_cnt = beat / 4;
-        _tick_cnt = (beat - _beat_cnt * 4) * 6;
-        _beat = calculatedBeat(_beat_cnt, _tick_cnt);
-        _isSPPChanged = true;
-    }
-
-    void tick() {
-        _tick_cnt++;
-        if (_tick_cnt == 24) {
-            _tick_cnt = 0;
-            _beat_cnt ++;
-        }
-        _beat = calculatedBeat(_beat_cnt, _tick_cnt);
-
-        auto t = daisy::System::GetNow();
-        auto delta = t - _ptime;
-        if (_ptime > 0) push(delta);
-        _ptime = t;
-        _tempo = tempo(avg());
-        checkDeviation(delta);
-    }
-
-    void push(uint32_t interval) {
-        _wndw[_iterator] = interval;
-        _iterator ++;
-        _iterator %= _wndw.size(); 
-        if (_iterator == 0) _filled = true;
-    }
-
-    float avg() {
-        float s = 0;
-        auto l = _filled ? _wndw.size() : _iterator + 1;
-        for (size_t i = 0; i < l; i++) s += _wndw[i];
-        _avg = s / l;
-        return _avg;
-    }
-
-    float tempo(float tick) {
-        return roundf(2500.f / tick);
-    }
-
-    void checkDeviation(uint32_t delta) {
-        if (delta > _avg - _dev_thres && delta < _avg + _dev_thres) {
-            _dev_cnt = 0;
-            return;
-        }
-        if (++_dev_cnt < _dev_cnt_thres) return;
-        
-        _iterator = 0;
-        _filled = false;
-        _dev_cnt = 0;
-    }
+    void start();
+    void stop();
+    void resume();
+    void seek(uint8_t bytes[2]);
+    void tick();
+    void push(uint32_t interval);
+    float avg();
+    float tempo(float tick);
+    void checkDeviation(uint32_t delta);
 };
