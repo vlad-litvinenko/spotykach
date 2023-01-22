9b350eaec19b5e9faeb4a88efc59737866e37b5a
Vlad <thatvlad@icloud.com>
2023-01-22 16:03:28 +0100

    Add precision

--- control/fcomp.h
+++ control/fcomp.h
@@ -1,4 +1,7 @@
 
-inline static bool fcomp(float lhs, float rhs) {
-    return int(lhs * 100) == int(rhs * 100);
+inline static bool fcomp(float lhs, float rhs, int precision = 2) {
+    auto digits = precision * 10;
+    auto lhs_int = static_cast<int>(roundf(lhs * digits));
+    auto rhs_int = static_cast<int>(roundf(rhs * digits));
+    return lhs_int == rhs_int;
 }
