660e84dbc43c3c58dd8a4f8a457c12e7176b250c
Vlad <thatvlad@icloud.com>
2023-04-07 14:30:07 +0200

    Persistence

---
+++ control/persistense.h
@@ -0,0 +1,57 @@
+#include "daisy_seed.h"
+
+Persistence::Params DSY_QSPI_BSS params_qspi;
+
+class Persistence {
+public:
+    Persistence() = default;
+    ~Persistence() = default;
+
+    initialize(daisy::DaisySeed& hw) {
+        _params = params_qspi;
+        _hw = &hw;
+    }
+
+    int cword_pattern_a() { return _params.cword_pattern_a; }
+    void set_cword_pattern_a(int value) { 
+        _params.cword_pattern_a = value;
+        store();
+    }
+
+    int cword_pattern_b() { return _params.cword_pattern_b; }
+    void set_cword_pattern_b(int value) { 
+        _params.cword_pattern_b = value;
+        store();
+    }
+
+    int even_pattern_a() { return _params.even_pattern_a; }
+    void set_even_pattern_a(int value) { 
+        _params.even_pattern_a = value;
+        store();
+    }
+
+    int even_pattern_a() { return _params.even_pattern_a; }
+    void set_even_pattern_b(int value) { 
+        _params.even_pattern_a = value;
+        store();
+    }
+
+private:
+    struct Params {
+	    int even_pattern_a;
+	    int cword_pattern_a;
+	    int even_pattern_b;
+	    int cword_pattern_b; 
+    };
+
+    void store() {
+        size_t size = sizeof(_params);
+	    size_t address = (size_t)(&params_qspi);
+	    
+	    _hw->qspi.Erase(address, address + size);
+	    _hw->qspi.Write(address, size, (uint8_t*)(&_params));    
+    }
+
+    daisy::DaisySeed* _hw;
+    Params _params;
+}
