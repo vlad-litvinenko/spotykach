e2e069be52ff17567d78f7f7ad96763106686fbc
Vlad <thatvlad@icloud.com>
2023-04-14 21:52:58 +0200

    Layout

--- control/layout.h
+++ control/layout.h
@@ -1,3 +1,12 @@
 #pragma once
 
 //#define ROEY_LAYOUT
+
+/*
+Actual layout is defined here:
+ Knobs:             control/knob.h ->               daisy::Pin pin(Knob::Target usage) const
+ Channel Toggles:   control/channeltoggles.h ->     daisy::Pin pin(Target t, Channel ch)
+ Global Toggles:    control/globaltoggles.h ->      daisy::Pin pin(Target t)
+ Sensors:           controls/descrete.sensor.h ->   void initialize()
+ LEDs:              control/leds.h ->               void initialize()
+*/
